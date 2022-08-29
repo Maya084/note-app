121501375330b91176819b00c3c92aa99c4fa773
Maya084 <37115588+Maya084@users.noreply.github.com>
2022-08-29 19:50:10 +0200

    :sparkles: (global) add home, note and header components

--- src/App.svelte
+++ src/App.svelte
@@ -1,45 +1,23 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  import Home from '@/pages/Home.svelte'
+  import Header from '@/components/Header.svelte'
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+<Header />
 
-  <div class="card">
-    <Counter />
-  </div>
+<Home />
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
+<style lang="scss">
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    height: 100%;
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
+
+  :global(body) {
+    /* this will apply to <body> */
+    margin: 0;
+    height: 100%;
+    background-color: #fbfbfb;
   }
 </style>
---
+++ src/pages/Home.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import Note from "@/components/Note.svelte";
+</script>
+
+<Note
+    id={1234}
+    title={"Some Note"}
+    content={"This note is about this and that"}
+    tags={["test", "text"]}
+    date={"20210806120000"}
+    isFavorite={false}
+/>
